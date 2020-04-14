b988a2147920a0d174b74a39d5d2af58a7f96d63
Iresharma <iresh.sharma8@gmail.com>
2020-04-15 04:44:39 +0530

    colors and themes

--- lib/globalvariables.dart
+++ lib/globalvariables.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:nie/Allfeed.dart';
 
-var AppColor = Colors.deepPurple;
+var AppColor = Appcolors[4];
 
-var Appolors = [
-	Colors.deepPurple,
-	Colors.lightGreen,
 
+var Appcolors = [
+	Colors.pinkAccent[400],
+	Colors.deepOrangeAccent[700],
+	Colors.yellow[400],
+	Colors.lightGreenAccent[700],
+	Colors.indigoAccent[200]
 ];
+
+var AccentColor = AccentColors[4];
+
+var AccentColors = [
+	Colors.red,
+	Colors.blue,
+	Colors.yellow,
+	Colors.green,
+	Colors.black38
+];
+
+var navcolors = Colors.white;
 
 var title = 'NIE';
 
-Widget body = Column();
+Widget body = Allfeed();
