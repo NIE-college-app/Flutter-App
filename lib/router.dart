a2c567acab8cc95f671f98cf45b1c9cee6050e0a
Iresharma <iresh.sharma8@gmail.com>
2020-04-19 15:12:28 +0530

    navigation fised

--- lib/router.dart
+++ lib/router.dart
@@ -4,24 +4,23 @@ import 'package:nie/AboutPost.dart';
 import 'package:nie/AboutProf.dart';
 import 'package:nie/Imagehandling.dart';
 import 'package:nie/contacts.dart';
-import 'package:nie/login.dart';
-import 'package:nie/loginPage.dart';
 import 'package:nie/settings.dart';
 import 'package:nie/timetable.dart';
+import 'package:nie/globalvariables.dart';
+import 'package:nie/login.dart';
+import 'package:nie/loginPage.dart';
 
-
+Widget landing = loginStatus ? Login() : loginPage();
 
 Map<String, Widget Function(BuildContext)> Router() {
 
-
 	var routes = {
-		'/': (_) => Login(),
+		'/': (_) => landing,
 		'/settings': (_) => Settings(),
 		'/timetable': (_) => TimeTable(),
 		'/imageUpload': (_) => ImageUplaod(),
 		'/contacts': (_) =>  Contacts(),
 		'/aboutProf': (_) => AboutProf(),
-		'/loginE' : (_) => loginPage(),
 		'/aboutPost':(_)=>AboutPost(),
 	};
 	return routes;
