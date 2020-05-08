fa296e80b9796c9fd369e3d091855719d18b7bd7
Iresharma <iresh.sharma8@gmail.com>
2020-05-08 19:31:14 +0530

    redesign 2

--- lib/contacts.dart
+++ lib/contacts.dart
@@ -55,7 +55,7 @@ class _ContactTileState extends State<ContactTile> {
           Navigator.pushNamed(
               context,
               '/aboutProf',
-              arguments: ScreenArgs(
+              arguments: ProfData(
                   widget.data.keys.toList()[0],
                   widget.data.values.toList()[0],
               )
