9ab440f93fb7c5fe418261f58092babb7605d883
Iresharma <iresh.sharma8@gmail.com>
2020-05-13 05:41:47 +0530

    still redesigning

--- lib/options.dart
+++ lib/options.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_progress_button/flutter_progress_button.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:nie/loginPage.dart';
 import 'package:nie/services/auth.dart';
+import 'package:nie/services/loader.dart';
 
 import 'globalvariables.dart';
 
@@ -124,13 +127,95 @@ class Options extends StatelessWidget {
                     text: 'Logout',
                     hasNavigation: false,
                   ),
-                  onTap: () => signOut(),
+                  onTap: () => _openSignOutBottomSheet(context),
                 )
               ],
             ),
           )
         ],
       ),
+    );
+  }
+
+  void _openSignOutBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+        shape: BottomSheetShape(),
+        backgroundColor: AppColor,
+        context: context,
+        builder: (_) {
+          return Container(
+            padding: EdgeInsets.only(top: 24,left: 49, right: 48),
+            height: 200,
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    "Laura, are you sure you want to sign out?",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Expanded(
+                          child: ProgressButton(
+                            defaultWidget: Text(
+                              "Sign out",
+                              style: TextStyle(
+                                color: AppColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            progressWidget: ColorLoader2(),
+                            color: Colors.white,
+                            onPressed: () async {
+                              await signOut();
+                              Future.delayed(Duration(milliseconds: 2000), () {
+                                Navigator.of(context).pushReplacement(
+                                    new MaterialPageRoute(builder: (_) => loginPage())
+                                );
+                              });
+                            },
+                          )
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Expanded(
+                        child: OutlineButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          borderSide: BorderSide(
+                            color: Theme.of(context).scaffoldBackgroundColor,
+                          ),
+                          color: Theme.of(context).primaryColor,
+                          child: Text(
+                            "Stay logged in",
+                            style: TextStyle(
+                              color: Theme.of(context).scaffoldBackgroundColor,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                ],
+              ),
+            ),
+          );
+        }
     );
   }
 }
@@ -189,4 +274,45 @@ class ProfileListItem extends StatelessWidget {
       ),
     );
   }
+}
+
+
+class BottomSheetShape extends ShapeBorder {
+  @override
+  // TODO: implement dimensions
+  EdgeInsetsGeometry get dimensions => throw UnimplementedError();
+
+  @override
+  Path getInnerPath(Rect rect, {TextDirection textDirection}) {
+    // TODO: implement getInnerPath
+    throw UnimplementedError();
+  }
+
+  @override
+  Path getOuterPath(Rect rect, {TextDirection textDirection}) {
+    return getClip(rect.size);
+  }
+
+  @override
+  void paint(Canvas canvas, Rect rect, {TextDirection textDirection}) {
+    // TODO: implement paint
+  }
+
+  @override
+  ShapeBorder scale(double t) {
+    // TODO: implement scale
+    throw UnimplementedError();
+  }
+
+  Path getClip(Size size) {
+    Path path = Path();
+
+    path.moveTo(0, 40);
+    path.quadraticBezierTo(size.width/2, 0, size.width, 40);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+
+    return path;
+  }
+
 }
